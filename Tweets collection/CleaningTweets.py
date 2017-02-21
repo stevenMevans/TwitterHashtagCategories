68c999595ab9b9e2f7d51f654fb17b1aa75cac1b
Balaji Natarajan <balaji.natarajan.7@gmail.com>
2017-02-21 16:20:23 -0600

    Create CleaningTweets.py

---
+++ Tweets collection/CleaningTweets.py
@@ -0,0 +1,11 @@
+f = open('fetched_tweets_op.json')
+ff = open('STEP1_OP.json','a')
+while True:
+	a = f.readline()
+	print(a)
+	if a == '\n' or len(a)<60:
+		continue
+	else:
+		ff.write(a)
+f.close()
+ff.close()

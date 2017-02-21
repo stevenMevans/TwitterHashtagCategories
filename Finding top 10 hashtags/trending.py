fed746ed02380c6440f4654c8dc4170d59620db8
Balaji Natarajan <balaji.natarajan.7@gmail.com>
2017-02-21 16:26:31 -0600

    Update trending.py

--- Finding top 10 hashtags/trending.py
+++ Finding top 10 hashtags/trending.py
@@ -3,38 +3,32 @@ def main():
 	i=1
 	dic = dict()
 	while True:
+		# manipulating each line
 		line = file.readline()
-		#print(line)
-
 		line = line.split(',')
 		if len(line) > 4:
-			#print(line[3])
+			# storing text attribute of tweet in a
 			a = line[3]
-			#print(a[8:-1])
 			words = str(line[3]).split()
 			for e in words:
+				# Finding words starting with '#' and if so then store in a dictionary (hash table)
 				if e.startswith('#'):
-					#print(e)
 					if e in dic.keys():
 						dic[e] += 1
 					else:
 						dic[e] = 1
 		if len(line)<=1:
 			break
-	#print(dic)
-
 	d = dic
 	o = list()
 	s = [(k, d[k]) for k in sorted(d, key=d.get, reverse=True)]
 	for k, v in s:
 		o.append((k, v))
-	
 	op = o[0:10]
 	f = open('TOP_10_TWEETS.txt','a')
 	for i in op:	
 		f.write(str(i)+'\n')
 	f.close()
-	
 
 if __name__=='__main__':
 	main()

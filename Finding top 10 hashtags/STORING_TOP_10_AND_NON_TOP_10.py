8bfc4bb3e8e5e91132b33c492bbd9042bd6d79c3
Balaji Natarajan <balaji.natarajan.7@gmail.com>
2017-02-26 17:53:39 -0600

    Create STORING_TOP_10_AND_NON_TOP_10.py

---
+++ Finding top 10 hashtags/STORING_TOP_10_AND_NON_TOP_10.py
@@ -0,0 +1,31 @@
+def main():
+	f = open('MAP_REDUCE_OUTPUT.txt')
+	hashtable = {}
+	while True:
+		inp = f.readline()
+		l = inp.split()
+		if len(l) == 2:
+			hashtable[l[0]] = int(l[1])
+
+		if len(inp)<=1:
+			break
+	d = hashtable
+	o = list()
+	s = [(k, d[k]) for k in sorted(d, key=d.get, reverse=True)]
+	for k, v in s:
+		o.append((k, v))
+	
+	op = o[0:10]
+	f = open('TOP_10_TWEETS1.txt','w')
+	for i in op:	
+		f.write(str(i)+'\n')
+	f.close()
+	
+	op1 = o[10:]
+	f1 = open('AFTER_TOP_10_TWEETS.txt','w')
+	for i in op1:	
+		f1.write(str(i)+'\n')
+	f1.close()
+
+if __name__=='__main__':
+	main()
